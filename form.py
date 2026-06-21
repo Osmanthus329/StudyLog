71396c91b51dd48fd47643dfc7a9f1e684e607b8
Osmanthus329 <xiaoyeh04@gmail.com>
2026-06-21 15:26:11 +0900

    Create MyTaks form.

--- form.py
+++ form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,EmailField,TextAreaField
+from wtforms import StringField,PasswordField,SubmitField,EmailField,TextAreaField,DateField
 from wtforms.validators import DataRequired
 
 class Register(FlaskForm):
@@ -17,4 +17,12 @@ class Login(FlaskForm):
 class MySubject(FlaskForm):
     subject_name = StringField(name = "subject",validators=[DataRequired()])
     description = TextAreaField(name = "description")
+    submit = SubmitField(name = "submit")
+
+class MyTask(FlaskForm):
+    task_name = StringField(name = "task",validators=[DataRequired()])
+    deadline = DateField(name = "deadline",validators=[DataRequired()])
+    priority = StringField(name = "priority")
+    status = StringField(name = "status")
+    memo = TextAreaField(name = "memo")
     submit = SubmitField(name = "submit")
